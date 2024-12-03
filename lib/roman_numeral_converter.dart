28a7a768ccfbac3b689ad6893613efe8c89347cf
Keegan Fontaine <keegsfontaine@gmail.com>
2024-12-03 10:16:43 -0500

    Implement the convertToInt method
    Implementation of the convertToInt method on the RomanNumeralConverter class is working. The code likely needs to be cleaned-up, however.

--- lib/roman_numeral_converter.dart
+++ lib/roman_numeral_converter.dart
@@ -1,5 +1,37 @@
 class RomanNumeralConverter {
-  convertFromInt(final String numeral) {
-    return 1;
+  final Map<String, int> numerals = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
+
+  int convertToInt(final String numeral) {
+    int runningSum = 0;
+
+    for (int i = 0; i < numeral.length - 1; i++) {
+      final currentChar = numeral[i];
+      final nextChar = numeral[i + 1];
+
+      final currentNumeralValue = numerals[currentChar];
+      final nextNumeralValue = numerals[nextChar];
+
+      if (currentNumeralValue == null || nextNumeralValue == null) {
+        throw Exception("Invalid Numeral Input: $numeral");
+      }
+
+      if (currentNumeralValue < nextNumeralValue) {
+        runningSum -= currentNumeralValue;
+      } else {
+        runningSum += currentNumeralValue;
+      }
+    }
+
+    runningSum += numerals[numeral.split("").last] ?? 0;
+
+    return runningSum;
   }
 }
